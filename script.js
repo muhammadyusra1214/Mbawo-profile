c71b10c9218d439e3a81a60e32f4574ee1443e80
muhammadyusra1214 <muhammadyusrah832@gmail.com>
2026-09-24 10:27:43 -0800

    added a new profile

---
+++ script.js
@@ -0,0 +1,12 @@
+const subtitle = document.querySelector("#subtitle");
+
+subtitle.textContent = "I am from Kano State"
+
+const button = document.createElement("button");
+button.textContent = "click me"
+
+const about = document.querySelector("#about");
+about.appendChild(button)
+
+const water = document.querySelector("#water");
+water. forEach(element=>)
